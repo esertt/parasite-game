33bcdd397af23637de272fca6e8b270a51dc760c
Eser Tekin Tekeli <73229848+esertt@users.noreply.github.com>
2025-10-03 22:18:43 +0300

    movement first control
    prototype for the first player movement.

---
+++ ParasiteEscape/Assets/scripts/puzzleAction.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class puzzleAction : MonoBehaviour
+{
+    private bool isInfected;
+    public float speed = 5.0f;
+    public Transform movePoint;
+    public Tilemap tileMap;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isInfected = false;
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, speed*Time.deltaTime);
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0f)
+        {
+            //first update the desire path
+            Vector3 newPos = movePoint.position;
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+            {
+                newPos = movePoint.position +  new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f );
+            }
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+            {
+                newPos = movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
+            
+            //check the desired path. If player can move then update the movePosition
+            Vector3Int cellPos = tileMap.WorldToCell(newPos);
+            TileBase tile = tileMap.GetTile(cellPos);
+            if (tile != null && tile.name == "tiletest_1")
+            {
+                Debug.Log("Tile found at " + cellPos + ": " + tile.name);
+            }
+            else
+            {
+                movePoint.position = newPos;
+            }
+        }
+    }
+}
